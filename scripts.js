c7e1438c7b5f46acb53f4980712d54e6974c67bc
Gergo MKV <gergo.tallai@gmail.com>
2022-07-22 11:51:30 +0200

    Objektumok, Arrays, Függvények

--- scripts.js
+++ scripts.js
@@ -1,4 +1,169 @@
+//#region Ciklusok
 
+
+
+
+
+//#endregion
+
+//#region Objektum metódusok
+/*
+var huba = {
+    nev: 'Huba',
+    kor: 37,
+    foglalkozas: 'vezér',
+    hazas: true,
+    baratok: ['Álmos', 'Előd', 'Ond'],
+    szuletesiEvSzamitas: function(){
+        this.szuletesiEv = 2022 - this.kor; 
+    }
+};
+
+huba.szuletesiEvSzamitas();
+console.log(huba);
+*/
+//#endregion
+
+//#region Objektumok, tulajdonságok
+/*
+//Objektum literál
+var huba = {
+    nev: 'Huba',
+    kor: 34,
+    foglalkozas: 'vezér',
+    hazas: true,
+    baratok: ['Álmos', 'Előd', 'Ond'],
+};
+console.log(huba.foglalkozas);
+console.log(huba['nev']);
+
+var h = 'hazas';
+console.log(huba[h]);
+
+//Objektum
+var tas = new Object();
+tas.nev = 'Tas';
+tas.kor = 32;
+tas['foglalkozas'] = 'vezér';
+
+console.log(tas)
+*/
+//#endregion
+
+//#region Tömbök, Arrays
+/*
+var nev1 = 'Ond';
+var nev2 = 'Kond';
+var nev3 = 'Tas';
+
+var nevek = [nev1, nev2, nev3]; //Javasolt megoldás
+var korok = new Array(23,53,56);
+
+console.log(nevek[0]);
+console.log(nevek);
+console.log(nevek.length);
+
+nevek[1] = 'Huba';
+console.log(nevek);
+
+nevek[nevek.length] = 'Károly';
+console.log(nevek);
+
+var huba = ['huba', 34, 'vezér', true];
+
+//Hozzáadás
+huba.push(42); //végére ad értéket
+huba.unshift('ifj') //Tömb elejére adja az értéket
+console.log(huba);
+
+//Törölni
+huba.pop(); //utolsó elem a tömbből kitörli
+huba.shift(); // legelső 
+
+//Keresés
+console.log(huba.indexOf(34)); //ha nem talál: -1
+
+var szakacs = huba.indexOf('szakacs') === -1 ? 'Huba nem szakacs' : 'Huba szakacs' //Ternális
+console.log(szakacs)
+*/
+//#endregion
+
+//#region Függvények II
+/*
+var eletsSzakaszok = function(nev, kor){
+    switch(true){
+        case kor < 13:
+            return nev + ' gyerek';
+            break;
+        case kor >= 13 && kor <= 21:
+            return nev + ' tini';
+            break;
+        default:
+            return nev + ' felnőtt';
+    }
+}
+
+console.log(eletsSzakaszok('Kati', 14));
+*/
+//#endregion
+
+//#region Függvények
+/*
+function hello(nev){
+    //alert('Hello ' + nev);
+    return('Hello ' + nev);
+}
+console.log(hello('Gizi'));
+
+function teglalapKeruletTerulet(a,b,funckio){
+    var eredmeny;
+    if (funckio ==='kerulet') {
+        eredmeny = (a + b)*2;
+    } else if (funckio === 'terulet'){
+        eredmeny = a * b;
+    } else {
+        return 'Hibás';
+    }
+
+    return funckio + ' = ' + eredmeny;
+}
+
+console.log(teglalapKeruletTerulet(5,4,'kerulet'));
+console.log(teglalapKeruletTerulet(5,4,'terulet'));
+console.log(teglalapKeruletTerulet(5,4,'aassd'));
+*/
+//#endregion
+
+//#region egyenlőség == és totális egyenlőség ===
+/*
+var szam;
+szam = 0;
+if (szam || szam === 0){
+    console.log('Létezik')
+}else{
+    console.log('Nem')
+}
+
+// == nem szigorú egyezőség (10='10') és === totális egyezőség
+var x = 10;
+if(x == '10'){
+    console.log('Igen')
+} else {
+    console.log('Nem')
+}
+*/
+//#endregion
+
+//#region MŰVELETEK SORRENDJE
+/*
+var kigyok = 100;
+var bekak = 21
+var madarak = 39;
+
+var tobbAHullo = kigyok + bekak > madarak;
+console.log(tobbAHullo);
+*/
+//#endregion
 
 //#region TERNÁLIS és SWITCH
 /*
